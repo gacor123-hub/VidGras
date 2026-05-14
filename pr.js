37ae6fc8a622489b4c82612fa8ec61a60c2785ea
gacor123-hub <linuxgacor123@gmail.com>
2026-05-15 01:48:05 +0700

    frontend:selesai......hore🎉

--- pr.js
+++ pr.js
@@ -1,15 +1,25 @@
 const navMenu = document.getElementById('navMenu');
-const hamburger = document.querySelector('.hamburger');
+// const hamburger = document.querySelector('.hamburger');//
+const kembali = document.querySelector('body')
 
 function toggleMenu() {
     navMenu.classList.toggle('active');
-    hamburger.classList.toggle('active');
+    // hamburger.classList.toggle('active');//
 }
 
 // Menutup menu saat link diklik
 document.querySelectorAll('#navMenu a').forEach(link => {
     link.addEventListener('click', () => {
         navMenu.classList.remove('active');
-        hamburger.classList.remove('active');
+        // hamburger.classList.remove('active');//
     });
 });
+
+document.querySelectorAll('body').addEventListener('click', () => {
+    kembali.tagName.remove('active')
+    
+})
+
+function bodyRemove(){
+
+}
