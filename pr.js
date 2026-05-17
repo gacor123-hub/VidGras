432746f1ac3782fdd454c1ebd890b7443724febd
gacor123-hub <linuxgacor123@gmail.com>
2026-05-17 17:34:59 +0700

    fix:tampilan dan navbar tidak mengikuti layar di versi mobile

--- pr.js
+++ pr.js
@@ -1,25 +1,25 @@
 const navMenu = document.getElementById('navMenu');
-// const hamburger = document.querySelector('.hamburger');//
-const kembali = document.querySelector('body')
+const hamburger = document.querySelector('.hamburger');
 
 function toggleMenu() {
     navMenu.classList.toggle('active');
-    // hamburger.classList.toggle('active');//
+    hamburger.classList.toggle('active');
 }
 
 // Menutup menu saat link diklik
 document.querySelectorAll('#navMenu a').forEach(link => {
     link.addEventListener('click', () => {
         navMenu.classList.remove('active');
-        // hamburger.classList.remove('active');//
+        hamburger.classList.remove('active');//
     });
 });
 
-document.querySelectorAll('body').addEventListener('click', () => {
-    kembali.tagName.remove('active')
-    
+document.addEventListener('click', (e) => {
+    if (
+        !navMenu.contains(e.target) &&
+        !hamburger.contains(e.target)
+    ) {
+        navMenu.classList.remove('active');
+        hamburger.classList.remove('active');
+    }
 })
-
-function bodyRemove(){
-
-}
