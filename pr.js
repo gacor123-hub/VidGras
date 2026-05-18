dbcbf54da975c26d175bdf0b62040c32bb27968d
gacor123-hub <linuxgacor123@gmail.com>
2026-05-18 19:05:26 +0700

    modified and deleted

--- pr.js
+++ pr.js
@@ -3,14 +3,14 @@ const hamburger = document.querySelector('.hamburger');
 
 function toggleMenu() {
     navMenu.classList.toggle('active');
-    hamburger.classList.toggle('active');
+    // hamburger.classList.toggle('active');
 }
 
 // Menutup menu saat link diklik
 document.querySelectorAll('#navMenu a').forEach(link => {
     link.addEventListener('click', () => {
         navMenu.classList.remove('active');
-        hamburger.classList.remove('active');//
+        // hamburger.classList.remove('active');
     });
 });
 
